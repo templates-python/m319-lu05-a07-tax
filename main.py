c449b319c33ce86f295eb91bcced2e528798c49a
Marcel Suter <marcel.suter@bzz.ch>
2023-07-21 08:23:51 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 def main():
-    # Write your program here and remove the line "pass"
-    pass
+    tax = 0
+    TAX_EXEMPTION_LIMIT = 325000
+
+    inheritance = int(input('Inheritance:\n'))
+    taxable_amount = inheritance - TAX_EXEMPTION_LIMIT
+    years = int(input('Years since death:\n'))
+
+    if taxable_amount > 0:
+        if years < 3:
+            tax = taxable_amount * 0.40
+        elif years < 4:
+            tax = taxable_amount * 0.32
+        elif years < 5:
+            tax = taxable_amount * 0.24
+        elif years < 6:
+            tax = taxable_amount * 0.16
+        elif years < 7:
+            tax = taxable_amount * 0.08
+
+    print(f'Tax: {tax}')
 
 
 if __name__ == '__main__':
