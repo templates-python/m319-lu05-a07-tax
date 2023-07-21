68c0093d4d53ecb39fa2001434a7dd2fcc5435f7
Marcel Suter <marcel.suter@bzz.ch>
2023-07-21 08:04:33 +0200

    initial commit

---
+++ main_test.py
@@ -0,0 +1,68 @@
+import random
+
+from main import main
+
+
+def test_free1(capsys, monkeypatch):
+    amount = random.randrange(100000, 324999)
+    inputs = iter([amount, 2])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == 'Tax: 0\n'
+
+def test_free2(capsys, monkeypatch):
+    amount = random.randrange(325000, 999999)
+    inputs = iter([amount, 7])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == 'Tax: 0\n'
+
+def test_40(capsys, monkeypatch):
+    amount = random.randrange(325000, 999999)
+    tax = (amount - 325000) * 0.4
+    inputs = iter([amount, 2])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == f'Tax: {tax}\n'
+
+def test_32(capsys, monkeypatch):
+    amount = random.randrange(325000, 999999)
+    tax = (amount - 325000) * 0.32
+    inputs = iter([amount, 3.5])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == f'Tax: {tax}\n'
+
+
+def test_24(capsys, monkeypatch):
+    amount = random.randrange(325000, 999999)
+    tax = (amount - 325000) * 0.24
+    inputs = iter([amount, 4.1])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == f'Tax: {tax}\n'
+
+
+def test_16(capsys, monkeypatch):
+    amount = random.randrange(325000, 999999)
+    tax = (amount - 325000) * 0.16
+    inputs = iter([amount, 5.75])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == f'Tax: {tax}\n'
+
+
+def test_8(capsys, monkeypatch):
+    amount = random.randrange(325000, 999999)
+    tax = (amount - 325000) * 0.08
+    inputs = iter([amount, 6.25])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == f'Tax: {tax}\n'
